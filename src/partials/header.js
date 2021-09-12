d87139baeabbd79288a300580dba3888606fcfa9
Cologler <skyoflw+github@gmail.com>
2021-09-12 19:36:21 +0800

    refactor

--- src/partials/header.js
+++ src/partials/header.js
@@ -3,10 +3,39 @@
 var MiZ_MissingGrantAction;
 
 /**
+ *
+ * @param {string} apiName
+ * @returns
+ */
+function _MiZ_isGmApiAccessable(apiName) {
+    if (typeof apiName !== 'string') {
+        throw new Error('apiName must be a string');
+    }
+
+    if (!apiName.startsWith('GM')) {
+        throw new Error('apiName must start with "GM"');
+    }
+
+    var parts = apiName.split('.');
+    var obj = window;
+    for (const part of parts) {
+        obj = obj[part];
+        if (obj === undefined) {
+            return false;
+        }
+    }
+    return true;
+}
+
+/**
  * This is for internal use only and may change without notice.
  * @param {*} apiName
  */
-function _MiZ_OnMissingGrant(apiName) {
+function _MiZ_checkGrant(apiName) {
+    if (_MiZ_isGmApiAccessable(apiName)) {
+        return;
+    }
+
     const action = typeof MiZ_MissingGrantAction === 'undefined' ? null : MiZ_MissingGrantAction;
     const scriptName = GM.info.script.name;
     const message =
